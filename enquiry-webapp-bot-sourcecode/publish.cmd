482e4fd7a6bc227718befe62ca6a26d21571ffbd
Nafeesa-Kouser <65774281+Nafeesa-Kouser@users.noreply.github.com>
2022-03-30 22:38:03 +0530

    Add files via upload

---
+++ enquiry-webapp-bot-sourcecode/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\enquiry-webapp-bot-Web-Deploy.pubxml -p:Password=g8zPH3SnvlwsnweBsrKs4iadv8MZuklfgHNFTJnaWKDAHwCtxs6Srqok2pZd
+
